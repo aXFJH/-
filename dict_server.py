2995fb563c75b5fadb2923484d07c944ecc08ae7
aXFJH <1076548878@qq.com>
2022-06-04 09:40:28 +0800

    第二次修改

--- dict_server.py
+++ dict_server.py
@@ -5,16 +5,20 @@ host='0.0.0.0'
 port=8888
 ADDR=(host,port)
 def main():
-    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM,1)
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     s.bind(ADDR)
     s.listen(5)
-    # signal.signal(signal.SIGCHLD,signal.SIG_IGN)
+    signal.signal(signal.SIGCHLD,signal.SIG_IGN)
     print("listen from 8888.....")
     while True:
-        c,addr = s.accept()
-        print("连接地址：%s"%str(addr))
-        msg="欢迎访问主机8888"
-        c.send(msg.encode('utf-8'))
+        try:
+            c,addr = s.accept()
+            print("连接地址：%s"%str(addr))
+            msg="欢迎访问主机8888"
+            c.send(msg.encode('utf-8'))
+        except KeyboardInterrupt:
+            s.close()
+            sys.exit("服务退出")
         # c.close()
 if __name__ == '__main__':
     main()
